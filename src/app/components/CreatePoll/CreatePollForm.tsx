c7775b9e1e437b12a71ff2794dfcdd632933cbd3
Matheus Jorge <mrljorge@outlook.com>
2024-02-02 17:28:54 -0300

    add : error functionality to Input

--- src/app/components/CreatePoll/CreatePollForm.tsx
+++ src/app/components/CreatePoll/CreatePollForm.tsx
@@ -3,24 +3,26 @@
 import { useEffect, useState } from "react";
 import { createPoll } from "./actions";
 import { useForm } from "react-hook-form";
-import { CreatePollFields, createPollSchema } from "./validation";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { createPollSchema } from "./validation";
+import type { CreatePollFields } from "./validation";
+import { Input } from "../Input";
+import { useUser } from "@clerk/nextjs";
 
 export function CreatePoll() {
+  const { user } = useUser();
+
   const form = useForm<CreatePollFields>({
     resolver: zodResolver(createPollSchema),
-    defaultValues: { title: "hello" },
+    defaultValues: {
+      userId: user?.id,
+    },
   });
 
   const [numOfOptions, setNumOfOptions] = useState(2);
 
-  async function onFormSubmit(fields: CreatePollFields) {
-    await createPoll(fields);
-  }
-
   function deleteOption() {
     if (numOfOptions <= 2) return;
-
     setNumOfOptions(numOfOptions - 1);
   }
 
@@ -32,41 +34,62 @@ export function CreatePoll() {
 
   for (let index = 1; index <= numOfOptions; index++) {
     optionInputs.push(
-      <>
-        <label htmlFor={`option${index}`}>Option {index}</label>
-        <input {...form.register(`option${index}` as keyof CreatePollFields)} />
-      </>,
+      <Input
+        key={index}
+        labelProps={{ text: `Option ${index}` }}
+        inputProps={{
+          ...form.register(`option${index}` as keyof CreatePollFields),
+        }}
+        error={
+          form.formState.errors[`option${index}` as keyof CreatePollFields]
+            ?.message
+        }
+      />,
     );
   }
 
   useEffect(() => {
-    console.log(form.getValues(), form.formState);
-  }, [form.formState]);
+    console.log(form.formState.errors);
+  }, [form.formState.errors]);
 
   return (
     <form
-      className="flex w-96 flex-col gap-4 rounded-xl border border-neutral-800 bg-black p-4 shadow-md"
-      onSubmit={form.handleSubmit(onFormSubmit)}
+      className="flex w-96 flex-col overflow-auto rounded-xl border border-neutral-800 bg-black p-4 shadow-md"
+      onSubmit={form.handleSubmit(async (data) => await createPoll(data))}
     >
-      <label htmlFor="title">Title</label>
-      <input {...form.register("title")} />
+      <input type="hidden" {...form.register("userId")} />
 
-      <label htmlFor="description">Description</label>
-      <input {...form.register("description")} />
+      <Input
+        labelProps={{ text: "Title" }}
+        inputProps={{ ...form.register("title") }}
+        error={form.formState.errors.title?.message}
+      />
+
+      <Input
+        labelProps={{ text: "Description" }}
+        inputProps={{ ...form.register("description") }}
+        error={form.formState.errors.description?.message}
+      />
 
       {optionInputs.map((option) => {
         return option;
       })}
 
-      <button type="button" onClick={addOption}>
-        add
-      </button>
+      <div className="flex flex-row items-center justify-between">
+        <button className="text-green-500" type="button" onClick={addOption}>
+          add
+        </button>
+        <button className="text-red-500" type="button" onClick={deleteOption}>
+          delete
+        </button>
+      </div>
 
-      <button type="button" onClick={deleteOption}>
-        delete
+      <button
+        className="mx-auto w-fit rounded-lg bg-purple-500 px-4 py-2"
+        type="submit"
+      >
+        submit
       </button>
-
-      <button type="submit">submit</button>
     </form>
   );
 }
---
+++ src/app/components/Input.tsx
@@ -0,0 +1,33 @@
+export function Input({
+  wrapperProps,
+  labelProps,
+  inputProps,
+  error,
+}: {
+  wrapperProps?: React.ComponentProps<"div">;
+  labelProps: React.ComponentProps<"label"> & { text?: string };
+  inputProps: React.ComponentProps<"input">;
+  error?: string;
+}) {
+  return (
+    <div className="flex flex-col p-0.5" {...wrapperProps}>
+      <label htmlFor={inputProps.name} {...labelProps}>
+        {labelProps.text}
+      </label>
+      <input
+        id={inputProps.name}
+        autoComplete="off"
+        className={error && "border-red-500"}
+        {...inputProps}
+      />
+      <span
+        role="alert"
+        className={`min-h-5 origin-top transform text-sm text-red-500 transition-all
+          ${error ? "scale-y-100 opacity-100" : "scale-y-75 opacity-0"}
+        `}
+      >
+        {error}
+      </span>
+    </div>
+  );
+}
