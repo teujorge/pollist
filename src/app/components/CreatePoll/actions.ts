1a0e8eb5cecfe224fb7c3dabb32d3af24373d0eb
Matheus Jorge <mrljorge@outlook.com>
2024-02-02 17:28:36 -0300

    fix : to create poll user relation is now required

--- src/app/components/CreatePoll/actions.ts
+++ src/app/components/CreatePoll/actions.ts
@@ -1,28 +1,22 @@
-"use server"
+"use server";
 
 import { db } from "@/database/db";
 import { redirect } from "next/navigation";
-import { CreatePollFields } from "./validation";
+import type { CreatePollFields } from "./validation";
 
-export async function createPoll(fields:CreatePollFields) {
-    
+export async function createPoll(fields: CreatePollFields) {
+  if (fields.userId === "") return;
+  if (fields.title === "") return;
+  if (fields.description === "") return;
 
-    
+  const createdPoll = await db.poll.create({
+    data: {
+      userId: fields.userId,
+      title: fields.title,
+      description: fields.description ?? "",
+      options: { create: [{ text: fields.option1 }, { text: fields.option2 }] },
+    },
+  });
 
-    if (fields.title === "") return;
-    if (fields.description === "") return;
-
-    const createdPoll = await db.poll.create({
-      data: {
-        title:fields.title,
-        description:fields.description??"",
-        options: {
-          create: [{ text: fields.option1 }, { text: fields.option2 }],
-        },
-      },
-    });
-
-    if (createdPoll) {
-      redirect(`/post/${createdPoll.id}`);
-    }
-  }
+  if (createdPoll) redirect(`/post/${createdPoll.id}`);
+}
--- src/app/components/CreatePoll/validation.ts
+++ src/app/components/CreatePoll/validation.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
 
 export const createPollSchema = z.object({
-    title:z.string().min(1,"Title is required"),
-    description:z.string().optional(),
-    option1:z.string().min(1,"Option is required"),
-    option2:z.string().min(1,"Option is required"),
-})
+  userId: z.string().min(1, "User ID is required"),
+  title: z.string().min(1, "Title is required"),
+  description: z.string().optional(),
+  option1: z.string().min(1, "Option is required"),
+  option2: z.string().min(1, "Option is required"),
+});
 
-export type CreatePollFields = z.infer<typeof createPollSchema>
+export type CreatePollFields = z.infer<typeof createPollSchema>;
