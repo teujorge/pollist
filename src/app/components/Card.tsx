4eb20e754dcc6b8b330e8f6265b8b52f9a843d20
Matheus Jorge <mrljorge@outlook.com>
2024-02-03 00:22:26 -0300

    add : Card component

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,17 @@
+export function Card({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={`flex w-fit flex-grow flex-col gap-2 rounded-xl border border-neutral-800 px-3 py-2
+        ${className}
+      `}
+    >
+      {children}
+    </div>
+  );
+}
