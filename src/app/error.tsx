6780b66351e8a620e3c1112131f24e4977c03ef2
Matheus Jorge <mrljorge@outlook.com>
2024-02-05 16:03:20 -0300

    add : global error page

---
+++ src/app/error.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex h-[calc(100dvh-64px)] w-full items-center justify-center">
+      <div className="flex w-fit flex-col items-center justify-center gap-2">
+        <h2 className="text-2xl">Oops, something went wrong!</h2>
+
+        <p>
+          {error.name}: {error.message}
+        </p>
+
+        <button
+          className="rounded-lg px-2 py-1 hover:bg-neutral-800"
+          onClick={() => reset()}
+        >
+          Try again
+        </button>
+      </div>
+    </main>
+  );
+}
