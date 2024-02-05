a1fce5f0c113e3f166d6c499ade22c046f59d102
Matheus Jorge <mrljorge@outlook.com>
2024-02-05 12:06:34 -0300

    add : poll page loading

---
+++ src/app/polls/[id]/loading.tsx
@@ -0,0 +1,13 @@
+export default function PollPageLoading() {
+  return (
+    <main className="flex min-h-full flex-col justify-center">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="bg-neutral-400">title</h1>
+          <span className="bg-neutral-400">Created by</span>
+        </div>
+        <div className="bg-neutral-400" />
+      </div>
+    </main>
+  );
+}
