0c6b78eef04b63ef2b49bb562cf24e9d4b426962
Matheus Jorge <mrljorge@outlook.com>
2024-02-05 16:01:23 -0300

    fix : attempt to fix console errors from clerk auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,26 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/welcome"],
-  ignoredRoutes: ["/api/user"],
-  // debug: true,
+  publicRoutes: ["/", "/welcome", "/api/user"],
+
+  afterAuth(auth) {
+    // allow access to  public routes
+    if (auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+
+    // allow authenticated users access to all routes
+    if (auth.userId) {
+      return NextResponse.next();
+    }
+
+    // redirect to login
+    return NextResponse.redirect("/");
+  },
 });
 
 export const config = {
