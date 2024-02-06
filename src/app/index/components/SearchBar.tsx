4452bdc727a0bf796612666ff58834acdeac92a7
Matheus Jorge <mrljorge@outlook.com>
2024-02-06 02:20:19 -0300

    add : infinite loading on home page

--- src/app/index/components/SearchBar.tsx
+++ src/app/index/components/SearchBar.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { Input } from "@/app/components/Input";
-import { ArrowSvg } from "@/app/svgs/ArrowSvg";
-import { usePagination } from "../hooks/usePagination";
+import { useSearch } from "../hooks/useSearch";
 
-export function SearchBar({ hasNext }: { hasNext: boolean }) {
-  const { page, setPage, setSearch } = usePagination();
+export function SearchBar() {
+  const { setSearch } = useSearch();
 
   return (
     <div className="flex w-fit flex-row items-center justify-center gap-1">
@@ -17,22 +16,6 @@ export function SearchBar({ hasNext }: { hasNext: boolean }) {
           onChange: (event) => setSearch(event.target.value),
         }}
       />
-      <button
-        className={`h-8 w-8 rotate-180 rounded-md p-2 transition-colors hover:bg-neutral-800
-          ${page <= 1 && "pointer-events-none cursor-default opacity-50"}
-        `}
-        onClick={() => setPage((prev) => prev - 1)}
-      >
-        <ArrowSvg className="h-full w-full fill-neutral-200" />
-      </button>
-      <button
-        className={`h-8 w-8 rounded-md p-2 transition-colors hover:bg-neutral-800
-          ${!hasNext && "pointer-events-none cursor-default opacity-50"}
-        `}
-        onClick={() => setPage((prev) => prev + 1)}
-      >
-        <ArrowSvg className="h-full w-full fill-neutral-200" />
-      </button>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,22 @@
-import { db } from "@/database/db";
-import { PollCard } from "./components/PollCard/PollCard";
 import { SearchBar } from "./index/components/SearchBar";
-
-const PAGE_SIZE = 10;
+import { InfinitePolls } from "./components/InfinitePolls/InfinitePolls";
 
 export default async function HomePage({
   searchParams,
 }: {
   searchParams: Record<string, string | string[] | undefined>;
 }) {
-  const dangerousPage = Math.max(
-    parseInt(searchParams?.page?.toString() ?? "1"),
-    1,
-  );
-  const page = isNaN(dangerousPage) ? 1 : dangerousPage;
   const search = searchParams?.search?.toString() ?? "";
-
-  const polls = await db.poll.findMany({
-    where: { title: { contains: search } },
-    skip: (page - 1) * PAGE_SIZE,
-    take: PAGE_SIZE,
-  });
 
   return (
     <main className="flex flex-col items-center gap-4 pt-0">
       <div className="h-4" />
       <div className="sticky top-10 z-10 flex w-full flex-row items-end justify-between border-b border-neutral-800 bg-black py-1 pt-6">
         <h1 className="text-4xl font-bold">Polls</h1>
-        <SearchBar
-          hasNext={polls.length % PAGE_SIZE === 0 && polls.length !== 0}
-        />
+        <SearchBar />
       </div>
-      {polls.map((poll) => (
-        <PollCard key={poll.id} pollId={poll.id} />
-      ))}
+
+      <InfinitePolls query={search} />
     </main>
   );
 }
