08b8a8b048449e491b275f2e6c82f857d04b48ad
Matheus Jorge <mrljorge@outlook.com>
2024-02-07 00:44:28 -0300

    fix : loading width

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "./components/Loader";
 
 export default function HomePageLoading() {
   return (
-    <main className="flex h-dvh w-dvw items-center justify-center bg-red-500">
+    <main className="flex h-dvh w-full items-center justify-center bg-red-500">
       <Loader />
     </main>
   );
