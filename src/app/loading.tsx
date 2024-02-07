17f5f8e788caf140521dd321abdba59227d0055a
Matheus Jorge <mrljorge@outlook.com>
2024-02-07 00:38:31 -0300

    fix : make default loading page stand out for debugging

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,21 +1,9 @@
-import { Input } from "./components/Input";
+import { Loader } from "./components/Loader";
 
 export default function HomePageLoading() {
   return (
-    <main className="flex flex-col items-center gap-4 pt-0">
-      <div className="h-4" />
-      <div className="sticky top-10 z-10 flex w-full flex-row items-end justify-between border-b border-neutral-800 bg-black py-1 pt-6">
-        <h1 className="text-4xl font-bold">Polls</h1>
-        <div className="flex w-fit flex-row items-center justify-center gap-1">
-          <Input
-            wrapperProps={{ className: "p-1" }}
-            inputProps={{
-              disabled: true,
-              placeholder: "Search for a poll",
-            }}
-          />
-        </div>
-      </div>
+    <main className="flex h-dvh w-dvw items-center justify-center bg-red-500">
+      <Loader />
     </main>
   );
 }
