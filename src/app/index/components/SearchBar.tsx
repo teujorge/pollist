aaf48c319ef872a7dac1507b2d3303163396f960
Matheus Jorge <mrljorge@outlook.com>
2024-02-07 00:38:15 -0300

    style : fix home page search bar css

--- src/app/index/components/SearchBar.tsx
+++ src/app/index/components/SearchBar.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import { Input } from "@/app/components/Input";
 import { useSearch } from "../hooks/useSearch";
 
 export function SearchBar() {
   const { setSearch } = useSearch();
 
   return (
-    <div className="flex w-fit flex-row items-center justify-center gap-1">
-      <Input
-        wrapperProps={{ className: "p-1" }}
-        inputProps={{
-          type: "text",
-          placeholder: "Search for a poll",
-          onChange: (event) => setSearch(event.target.value),
-        }}
-      />
-    </div>
+    <input
+      type="text"
+      placeholder="Search"
+      className="w-[calc(90%-90px)] max-w-fit"
+      onChange={(event) => setSearch(event.target.value)}
+    />
   );
 }
