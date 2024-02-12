af37b94946fb437431fdbbb3c387057c0df998f1
Matheus Jorge <mrljorge@outlook.com>
2024-02-11 23:29:53 -0300

    refactor : supabase migration

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,14 +1,9 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
-
-import { env } from "@/env.js";
+import { env } from "@/env";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-
-const client = new Client({ url: env.DATABASE_URL });
 
 export const db =
   globalForPrisma.prisma ??
@@ -19,7 +14,6 @@ export const db =
         : env.NODE_ENV === "development"
           ? ["warn", "error"]
           : ["error"],
-    adapter: new PrismaPlanetScale(client),
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
