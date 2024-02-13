4d1dcdc3c16734ec8c114078c68aa608ac067750
Matheus Jorge <mrljorge@outlook.com>
2024-02-13 18:01:11 -0300

    fix : category filter

--- src/app/components/InfinitePolls/actions.ts
+++ src/app/components/InfinitePolls/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { db } from "@/database/db";
-import { type Category, type PollQuery, PAGE_SIZE } from "@/constants";
+import { type PollQuery, PAGE_SIZE } from "@/constants";
 
 export type PollsDetails = NonNullable<
   Awaited<ReturnType<typeof getInfinitePolls>>
@@ -14,21 +14,50 @@ export async function getInfinitePolls({
   authorId,
   voterId,
 }: PollQuery & { page: number }) {
+  const isTrending = category === "trending";
+  const isControversial = category === "controversial";
+
+  const currentDate = new Date();
+  const recentDate = new Date(currentDate.setDate(currentDate.getDate() - 7)); // last 7 days
+
   const polls = await db.poll.findMany({
     where: {
       title: search ? { contains: search } : undefined,
+
       authorId: authorId ? { contains: authorId } : undefined,
-      votes: voterId
-        ? {
-            some: {
-              voterId: { contains: voterId },
-            },
-          }
-        : undefined,
-      ...trendingConditions(category),
-      ...controversialConditions(category),
+
+      votes: {
+        // filter by voterId
+        ...(voterId
+          ? {
+              some: {
+                voterId: { contains: voterId },
+              },
+            }
+          : {}),
+
+        // filter by date
+        ...(isTrending
+          ? {
+              some: {
+                createdAt: {
+                  gte: recentDate,
+                },
+              },
+            }
+          : {}),
+      },
+
+      ...(isControversial ? { controversial: true } : {}),
     },
-    orderBy: { createdAt: "desc" },
+    orderBy: isTrending
+      ? {
+          votes: {
+            _count: "desc",
+          },
+        }
+      : { createdAt: "desc" },
+
     skip: (page - 1) * PAGE_SIZE,
     take: PAGE_SIZE,
     include: {
@@ -39,29 +68,4 @@ export async function getInfinitePolls({
   });
 
   return polls;
-}
-
-function trendingConditions(category?: Category) {
-  if (category !== "trending") return {};
-
-  const currentDate = new Date();
-  const recentDate = new Date(currentDate.setDate(currentDate.getDate() - 7)); // last 7 days
-
-  return {
-    votes: {
-      some: {
-        createdAt: {
-          gte: recentDate,
-        },
-      },
-    },
-  };
-}
-
-function controversialConditions(category?: Category) {
-  if (category !== "controversial") return {};
-
-  return {
-    controversial: true,
-  };
 }
