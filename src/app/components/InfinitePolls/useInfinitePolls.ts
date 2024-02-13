ea45056ccde6fab1e03f97f6781c86474bbc70a9
Matheus Jorge <mrljorge@outlook.com>
2024-02-12 23:29:47 -0300

    refactor : simplify useInfinitePolls hook chain and fix issues

--- src/app/components/InfinitePolls/useInfinitePolls.ts
+++ src/app/components/InfinitePolls/useInfinitePolls.ts
@@ -1,47 +1,35 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+import { type PollsDetails, getInfinitePolls } from "./actions";
 import type { PollQuery } from "@/constants";
-import { getInfinitePolls, type PollsDetails } from "./actions";
+
+type Data = {
+  polls: PollsDetails;
+  page: number;
+  hasMore: boolean;
+  isLoading: boolean;
+};
+
+const initialData: Data = {
+  polls: [],
+  page: 1,
+  hasMore: true,
+  isLoading: false,
+};
 
 export function useInfinitePolls(props: {
   query: PollQuery;
   loaderRef: React.RefObject<HTMLElement>;
 }) {
-  const [data, setData] = useState<{
-    polls: PollsDetails;
-    page: number;
-    hasMore: boolean;
-    isLoading: boolean;
-  }>({ polls: [], page: 1, hasMore: true, isLoading: false });
+  const initRef = useRef(false);
 
-  const loadMore = useCallback(async () => {
-    const initialPage = data.page;
-    setData((prev) => ({ ...prev, isLoading: true }));
-
-    const newPolls = await getInfinitePolls({
-      page: data.page,
-      search: props.query.search,
-      category: props.query.category,
-      authorId: props.query.authorId,
-      voterId: props.query.voterId,
-    });
-
-    // If page has changed during async call, ignore this result
-    if (data.page < initialPage) return;
-
-    const hasMore = newPolls.length > 0;
-
-    setData((prev) => ({
-      ...prev,
-      hasMore: hasMore,
-      isLoading: false,
-      polls: [...prev.polls, ...newPolls],
-    }));
-  }, [data.page, props.query]);
+  const [data, setData] = useState(initialData);
 
   // Reset data when query changes
   useEffect(() => {
+    if (!initRef.current) return;
     // console.log("useEffect - query has changed");
     setData({
       polls: [],
@@ -53,6 +41,39 @@ export function useInfinitePolls(props: {
 
   // Change page when loaderRef is intersecting
   useEffect(() => {
+    async function loadMore() {
+      const initialData = data;
+
+      try {
+        setData((prev) => ({ ...prev, page: prev.page + 1, isLoading: true }));
+
+        const newPolls = await getInfinitePolls({
+          page: data.page,
+          search: props.query.search,
+          category: props.query.category,
+          authorId: props.query.authorId,
+          voterId: props.query.voterId,
+        });
+
+        // another query change happened
+        if (initialData.page !== data.page) return;
+
+        const hasMore = newPolls.length > 0;
+
+        setData((prev) => ({
+          ...prev,
+          hasMore: hasMore,
+          isLoading: false,
+          polls: [...prev.polls, ...newPolls],
+        }));
+
+        initRef.current = true;
+      } catch (e) {
+        console.error(e);
+        setData(initialData);
+      }
+    }
+
     // console.log("useEffect - loaderRef is intersecting");
     function handleObserver(entities: IntersectionObserverEntry[]) {
       // console.log("handleObserver - entities", entities);
@@ -64,7 +85,7 @@ export function useInfinitePolls(props: {
       if (!target || !target.isIntersecting) return;
 
       // console.log("handleObserver - PASSED conditions");
-      setData((prev) => ({ ...prev, page: prev.page + 1 }));
+      void loadMore();
     }
 
     const option = {
@@ -77,15 +98,7 @@ export function useInfinitePolls(props: {
     if (props.loaderRef.current) observer.observe(props.loaderRef.current);
 
     return () => observer.disconnect();
-  }, [data.hasMore, data.isLoading, props.loaderRef]);
-
-  // Load more when page changes
-  useEffect(() => {
-    if (!data.hasMore) return;
-    if (data.isLoading) return;
-    // console.log("useEffect - page has changed");
-    void loadMore();
-  }, [loadMore]);
+  }, [data, props.loaderRef, props.query]);
 
   return data;
 }
