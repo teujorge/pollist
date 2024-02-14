c8b93f8b8f8161fbe4c58b0ec2c296541ac9bbbc
Matheus Jorge <mrljorge@outlook.com>
2024-02-14 20:30:46 -0300

    fix : signed in users set to anon

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,90 +4,59 @@ import { useUser } from "@clerk/nextjs";
 import { createContext, useContext, useEffect, useState } from "react";
 import { checkAndCreateAnonUser } from "./api/anon/actions";
 
-export function App({ children }: { children: React.ReactNode }) {
-  const { user } = useUser();
-  const [userId, setUserId] = useState(user?.id);
-  const [isAnon, setIsAnon] = useState(user?.id === undefined);
-
-  useEffect(() => {
-    const isSafariMac = /Macintosh.*Safari/.test(navigator.userAgent);
-
-    if (isSafariMac) return;
-
-    const css = `
-        ::-webkit-scrollbar {
-          width: 9px;
-        }
-
-        ::-webkit-scrollbar-track {
-          box-shadow: inset 0 0 10px 10px transparent;
-          border-radius: 2rem;
-        }
-
-        ::-webkit-scrollbar-thumb {
-          border-radius: 1.5rem;
-          box-shadow: inset 0 0 10px 10px #444;
-          border: 3px solid transparent;
-          transition: all 150ms ease;
-        }
-
-        ::-webkit-scrollbar-thumb:hover {
-          box-shadow: inset 0 0 10px 10px #666;
-          border: 2px solid transparent;
-        }
-
-        ::-webkit-scrollbar-button {
-          display: none;
-        }
-      `;
-    const styleSheet = document.createElement("style");
-
-    styleSheet.innerText = css;
-    document.head.appendChild(styleSheet);
-  }, []);
-
-  useEffect(() => {
-    console.log("app -> user?.id", user?.id);
-
-    // If the user is logged in, set the user ID
-    if (user?.id) {
-      setUserId(user.id);
-      setIsAnon(false);
-    }
-    // If the user is not logged in, get ip address and create an anon user if needed
-    else {
-      checkAndCreateAnonUser()
-        .then((id) => {
-          if (!user?.id) {
-            setUserId(id);
-            setIsAnon(true);
-          }
-        })
-        .catch((err) => {
-          console.error(err);
-          setUserId(undefined);
-          setIsAnon(true);
-        });
-    }
-  }, [user]);
-
-  // log userId changes
-  useEffect(() => {
-    console.log("app -> userId", userId);
-  }, [userId]);
-
-  return <AppProvider value={{ userId, isAnon }}>{children}</AppProvider>;
-}
-
-type AppContextType = {
+type UserStatus = {
   userId: string | undefined;
-  isAnon: boolean;
+  isAnon: boolean | undefined;
 };
 
-const AppContext = createContext<AppContextType | undefined>(undefined);
+export function App({ children }: { children: React.ReactNode }) {
+  useCustomScrollbar();
+
+  const { user } = useUser();
+  const [userStatus, setUserStatus] = useState<UserStatus>({
+    userId: undefined,
+    isAnon: undefined,
+  });
+
+  useEffect(() => {
+    async function updateStatus() {
+      // If the user is logged in, set the userStatus
+      if (user?.id) {
+        setUserStatus({ userId: user.id, isAnon: false });
+      }
+
+      // If the user is not logged in, use IP Address
+      else {
+        if (
+          userStatus.userId === undefined &&
+          userStatus.isAnon === undefined
+        ) {
+          setUserStatus({ userId: undefined, isAnon: true });
+          return;
+        }
+
+        const id = await checkAndCreateAnonUser();
+        if (!user?.id) {
+          setUserStatus({ userId: id, isAnon: true });
+        }
+      }
+    }
+
+    void updateStatus();
+  }, [user]);
+
+  // log userStatus
+  useEffect(() => {
+    console.log("userStatus", userStatus);
+  }, [userStatus]);
+
+  return <AppProvider value={userStatus}>{children}</AppProvider>;
+}
+
+const AppContext = createContext<UserStatus | undefined>(undefined);
 
 type AppProviderProps = {
-  value: AppContextType;
+  value: UserStatus;
   children: React.ReactNode;
 };
 
@@ -104,4 +73,47 @@ export function useApp() {
     throw new Error("useApp must be used within an AppProvider");
   }
   return context;
+}
+
+function useCustomScrollbar() {
+  useEffect(() => {
+    const isSafariMac = /Macintosh.*Safari/.test(navigator.userAgent);
+    if (isSafariMac) return;
+
+    const css = `
+      ::-webkit-scrollbar {
+        width: 9px;
+      }
+
+      ::-webkit-scrollbar-track {
+        box-shadow: inset 0 0 10px 10px transparent;
+        border-radius: 2rem;
+      }
+
+      ::-webkit-scrollbar-thumb {
+        border-radius: 1.5rem;
+        box-shadow: inset 0 0 10px 10px #444;
+        border: 3px solid transparent;
+        transition: all 150ms ease;
+      }
+
+      ::-webkit-scrollbar-thumb:hover {
+        box-shadow: inset 0 0 10px 10px #666;
+        border: 2px solid transparent;
+      }
+
+      ::-webkit-scrollbar-button {
+        display: none;
+      }
+    `;
+
+    const styleSheet = document.createElement("style");
+    styleSheet.innerText = css;
+    document.head.appendChild(styleSheet);
+
+    return () => {
+      // Cleanup the style element on unmount
+      document.head.removeChild(styleSheet);
+    };
+  }, []);
 }
