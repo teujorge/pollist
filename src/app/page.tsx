e5004e0b3cd123f4ffe83cc140429059114701fe
Matheus Jorge <mrljorge@outlook.com>
2024-02-21 02:23:31 -0300

    test : dynamic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import dynamic from "next/dynamic";
 import { Loader } from "./components/Loader";
-import { Suspense } from "react";
 import { FilterBar } from "./index/components/FilterBar";
-import { InfinitePolls } from "./components/InfinitePolls/InfinitePolls";
+
+const InfinitePolls = dynamic(
+  () =>
+    import("./components/InfinitePolls/InfinitePolls").then(
+      (mod) => mod.InfinitePolls,
+    ),
+  {
+    loading: () => <Loader />,
+  },
+);
 
 export default function HomePage({
   searchParams,
@@ -21,13 +30,50 @@ export default function HomePage({
 
       <div className="h-4" />
 
-      <Suspense key={`${search}-${category}`} fallback={<Loader />}>
+      <div
+        key={`${search}-${category}`}
+        className="flex h-fit w-full flex-col items-center"
+      >
         <InfinitePolls
           search={search}
           category={category}
           idPrefix="home-page-polls"
         />
-      </Suspense>
+      </div>
     </main>
   );
 }
+
+// import { Loader } from "./components/Loader";
+// import { Suspense } from "react";
+// import { FilterBar } from "./index/components/FilterBar";
+// import { InfinitePolls } from "./components/InfinitePolls/InfinitePolls";
+
+// export default function HomePage({
+//   searchParams,
+// }: {
+//   searchParams: Record<string, string | string[] | undefined>;
+// }) {
+//   const search = searchParams?.search?.toString() ?? "";
+//   const category = searchParams?.category?.toString() ?? "";
+
+//   return (
+//     <main className="flex flex-col items-center gap-2">
+//       <div className="sticky top-10 z-20 flex w-full flex-row items-end justify-center gap-2 bg-gradient-to-b from-black from-80% pt-6 sm:items-center">
+//         <h1 className="text-4xl font-bold">Polls</h1>
+//         <div className="flex-grow" />
+//         <FilterBar />
+//       </div>
+
+//       <div className="h-4" />
+
+//       <Suspense key={`${search}-${category}`} fallback={<Loader />}>
+//         <InfinitePolls
+//           search={search}
+//           category={category}
+//           idPrefix="home-page-polls"
+//         />
+//       </Suspense>
+//     </main>
+//   );
+// }
