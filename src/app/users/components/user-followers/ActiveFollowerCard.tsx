f893ca68936ac3920980cd7faa2f6bb850bbdbdd
guimell <guimell11@hotmail.com>
2024-02-24 16:18:52 -0300

    overhaul: followers modal

--- src/app/users/components/user-followers/ActiveFollowerCard.tsx
+++ src/app/users/components/user-followers/ActiveFollowerCard.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@clerk/nextjs";
 import { ActiveFollowerCardActions } from "./ActiveFollowerCardActions";
 import type { User } from "@prisma/client";
+import { ProfileImage } from "@/app/components/ProfileImage";
+import { FollowButton } from "../FollowButton";
 
 export async function ActiveFollowerCard({
   userId,
@@ -12,9 +14,18 @@ export async function ActiveFollowerCard({
   const { userId: myId } = auth();
 
   return (
-    <p>
-      {follower.username}
-      {myId === userId && <ActiveFollowerCardActions follower={follower} />}
-    </p>
+    <div className="flex flex-row items-center justify-center gap-1">
+      <ProfileImage
+        src={follower.imageUrl ?? "public/profilleIcon.jpg"}
+        alt={""}
+        width={20}
+        height={20}
+      ></ProfileImage>
+      <p>
+        {follower.username}
+        {myId === userId && <ActiveFollowerCardActions follower={follower} />}
+      </p>
+      {<FollowButton userId={follower.id} />}
+    </div>
   );
 }
