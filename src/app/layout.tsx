5ee26a5ce29d519cee5bc1ba44a31282126c56f2
Matheus Jorge <mrljorge@outlook.com>
2024-02-28 14:26:34 -0300

    refactor : global suspense boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import "@/styles/globals.css";
 import GlobalLoading from "./loading";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Nunito_Sans } from "next/font/google";
-import { Suspense } from "react";
-import { Toaster } from "sonner";
-import { dark } from "@clerk/themes";
 import { App } from "./app";
+import { dark } from "@clerk/themes";
+import { Toaster } from "sonner";
+import { Suspense } from "react";
+import { Nunito_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const nunito = Nunito_Sans({
   subsets: ["latin"],
@@ -29,43 +29,45 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <Script
+      <Suspense fallback={<GlobalLoading />}>
+        {/* <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6132246468312218"
         crossOrigin="anonymous"
         strategy="lazyOnload"
       /> */}
 
-      <ClerkProvider
-        appearance={{
-          baseTheme: dark,
-          variables: {
-            colorPrimary: "#a855f7", // purple
-            colorDanger: "#cc3333", // red
-            colorSuccess: "#22c55e", // green
-            colorWarning: "#F59e0b", // amber
+        <ClerkProvider
+          appearance={{
+            baseTheme: dark,
+            variables: {
+              colorPrimary: "#a855f7", // purple
+              colorDanger: "#cc3333", // red
+              colorSuccess: "#22c55e", // green
+              colorWarning: "#F59e0b", // amber
 
-            colorAlphaShade: "#ffffff",
+              colorAlphaShade: "#ffffff",
 
-            colorTextOnPrimaryBackground: "#f1f1f1",
-            colorTextSecondary: "#f1f1f1",
+              colorTextOnPrimaryBackground: "#f1f1f1",
+              colorTextSecondary: "#f1f1f1",
 
-            // colorBackground: "#010101",
+              // colorBackground: "#010101",
 
-            colorInputText: "#f1f1f1",
-            colorInputBackground: "#191919",
-          },
-        }}
-      >
-        <body className={`font-sans ${nunito.variable}`}>
-          <App>
-            <Toaster richColors />
-            <Suspense fallback={<GlobalLoading />}>{children}</Suspense>
-            {crudPoll}
-            {welcome}
-          </App>
-        </body>
-      </ClerkProvider>
+              colorInputText: "#f1f1f1",
+              colorInputBackground: "#191919",
+            },
+          }}
+        >
+          <body className={`font-sans ${nunito.variable}`}>
+            <App>
+              <Toaster richColors />
+              {children}
+              {crudPoll}
+              {welcome}
+            </App>
+          </body>
+        </ClerkProvider>
+      </Suspense>
     </html>
   );
 }
