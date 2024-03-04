e101a607e1201bd35f254f0bae7c05a0634bfb8e
Matheus Jorge <mrljorge@outlook.com>
2024-03-04 02:12:01 -0300

    add : dynamic sitemap generation in route handler

---
+++ src/app/api/sitemap.xml/route.ts
@@ -0,0 +1,70 @@
+import { db } from "@/database/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const staticPages = ["/", "/welcome", "/privacy-policy"];
+  const dynamicPages = [
+    ...(await getPopularPollIds()).map((id) => `/polls/${id}`),
+    ...(await getPopularUserIds()).map((id) => `/users/${id}`),
+  ];
+
+  const pages = [...staticPages, ...dynamicPages];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+        .map(
+          (path) =>
+            `<url>
+              <loc>${`https://pollist.org${path}`}</loc>
+            </url>`,
+        )
+        .join("")}
+    </urlset>
+  `;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
+
+async function getPopularPollIds() {
+  const polls = await db.poll.findMany({
+    orderBy: {
+      votes: {
+        _count: "desc",
+      },
+    },
+    take: 20,
+  });
+
+  return polls.map((poll) => poll.id);
+}
+
+async function getPopularUserIds() {
+  const usersCreators = await db.user.findMany({
+    orderBy: {
+      polls: {
+        _count: "desc",
+      },
+    },
+    take: 10,
+  });
+
+  const usersFollowers = await db.user.findMany({
+    orderBy: {
+      followers: {
+        _count: "desc",
+      },
+    },
+    take: 10,
+  });
+
+  const combinedUsers = [...usersCreators, ...usersFollowers];
+
+  const users = new Set(combinedUsers.map((user) => user.id));
+
+  return Array.from(users);
+}
