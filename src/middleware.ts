48c9cecc16a24830890ac6510a5fde4a044585db
Matheus Jorge <mrljorge@outlook.com>
2024-03-04 03:08:14 -0300

    add : sitemap route to ignored routes in clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   // publicRoutes: ["/", "/welcome", "/api/user"],
+  ignoredRoutes: ["/api/sitemap.xml"],
 
   afterAuth(auth, req) {
     // allow all users access
