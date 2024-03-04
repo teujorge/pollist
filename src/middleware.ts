303a0a0f962e5b9fc524c569df5cf7ac61f3b5ee
Matheus Jorge <mrljorge@outlook.com>
2024-03-04 02:32:34 -0300

    fix : middleware 2.0

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   // publicRoutes: ["/", "/welcome", "/api/user"],
-  publicRoutes: ["/api/sitemap.xml"],
 
+  beforeAuth() {
+    // allow all requests to be processed
+    return NextResponse.next();
+  },
   afterAuth(auth, req) {
     // allow all users access
     return NextResponse.next();
