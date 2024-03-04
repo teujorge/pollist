7f0f0cd8dcab9eeb075da83b329ba35f79938d8c
Matheus Jorge <mrljorge@outlook.com>
2024-03-04 15:53:47 -0300

    fix : temporarily ignore all routes in clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,26 +4,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: (req) => req.url.includes("/"),
-  ignoredRoutes: ["/api/sitemap.xml"],
-  debug: true,
-
-  // afterAuth(auth, req) {
-  //   // allow access to  public routes
-  //   if (auth.isPublicRoute) {
-  //     return NextResponse.next();
-  //   }
-
-  //   // allow authenticated users access to all routes
-  //   if (auth.userId) {
-  //     return NextResponse.next();
-  //   }
-
-  //   // redirect to login
-  //   const url = req.nextUrl.clone();
-  //   url.pathname = "/";
-  //   return NextResponse.redirect(url);
-  // },
+  // publicRoutes: (req) => req.url.includes("/"),
+  // ignoredRoutes: ["/api/sitemap.xml"],
+  ignoredRoutes: (req) => req.url.includes("/"),
 });
 
 export const config = {
