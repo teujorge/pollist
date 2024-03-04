31a457f85c3984acc6bbd7f84c0cfdea884319ef
Matheus Jorge <mrljorge@outlook.com>
2024-03-04 02:23:17 -0300

    fix : add sitemap route to public route in auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ import { NextResponse } from "next/server";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   // publicRoutes: ["/", "/welcome", "/api/user"],
+  publicRoutes: ["/api/sitemap.xml"],
 
   afterAuth(auth, req) {
     // allow all users access
