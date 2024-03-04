767640a76399b0eceb0e36b0e09fe44facd98dcb
Matheus Jorge <mrljorge@outlook.com>
2024-03-04 15:16:10 -0300

    fix : try to make all routes public in clerk auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,29 @@
-import { withClerkMiddleware } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default withClerkMiddleware(() => {
-  return NextResponse.next();
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: (req) => req.url.includes("/"),
+  ignoredRoutes: ["/api/sitemap.xml"],
+
+  // afterAuth(auth, req) {
+  //   // allow access to  public routes
+  //   if (auth.isPublicRoute) {
+  //     return NextResponse.next();
+  //   }
+
+  //   // allow authenticated users access to all routes
+  //   if (auth.userId) {
+  //     return NextResponse.next();
+  //   }
+
+  //   // redirect to login
+  //   const url = req.nextUrl.clone();
+  //   url.pathname = "/";
+  //   return NextResponse.redirect(url);
+  // },
 });
-
-// import { authMiddleware } from "@clerk/nextjs";
-
-// // This example protects all routes including api/trpc routes
-// // Please edit this to allow other routes to be public as needed.
-// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-// export default authMiddleware({
-//   // publicRoutes: ["/", "/welcome", "/api/user"],
-//   ignoredRoutes: ["/api/sitemap.xml"],
-
-//   afterAuth(auth, req) {
-//     // allow all users access
-//     return NextResponse.next();
-
-//     // allow access to  public routes
-//     if (auth.isPublicRoute) {
-//       return NextResponse.next();
-//     }
-
-//     // allow authenticated users access to all routes
-//     if (auth.userId) {
-//       return NextResponse.next();
-//     }
-
-//     // redirect to login
-//     const url = req.nextUrl.clone();
-//     url.pathname = "/";
-//     return NextResponse.redirect(url);
-//   },
-// });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
