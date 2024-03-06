5161422d4208098c4907abd6ebfea32f8c2e216b
Matheus Jorge <mrljorge@outlook.com>
2024-03-06 11:44:57 -0300

    fix : use depricated clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,14 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { withClerkMiddleware } from "@clerk/nextjs";
 
 // docs : https://clerk.com/docs/references/nextjs/auth-middleware
-export default authMiddleware({
-  publicRoutes: () => true,
-  ignoredRoutes: ["/api/sitemap.xml"],
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
 });
+// export default authMiddleware({
+//   publicRoutes: () => true,
+//   ignoredRoutes: ["/api/sitemap.xml"],
+// });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
