30757ce44d050f94b8131a7978b645a738b11640
Matheus Jorge <mrljorge@outlook.com>
2024-03-06 15:09:23 -0300

    fix : back to new middleware...

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,11 @@
-import { NextResponse } from "next/server";
-import { withClerkMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
 
 // docs : https://clerk.com/docs/references/nextjs/auth-middleware
-export default withClerkMiddleware(() => {
-  return NextResponse.next();
+
+export default authMiddleware({
+  publicRoutes: () => true,
+  ignoredRoutes: ["/api/sitemap.xml"],
 });
-// export default authMiddleware({
-//   publicRoutes: () => true,
-//   ignoredRoutes: ["/api/sitemap.xml"],
-// });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
