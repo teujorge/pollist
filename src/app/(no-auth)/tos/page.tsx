36e421b82e50d55850892e5fa3270a1a0622ba83
Matheus Jorge <mrljorge@outlook.com>
2024-03-09 12:30:32 -0300

    add : privacy and tos routes

---
+++ src/app/(no-auth)/tos/page.tsx
@@ -0,0 +1,78 @@
+export default function TermsOfServicePage() {
+  return (
+    <main className="gap-2 [&>h2]:pt-10 [&>h2]:text-3xl [&>h2]:font-semibold [&>p]:py-2 [&>ul]:list-disc [&>ul]:pl-6">
+      <div className="py-20">
+        <h1 className="py-4 text-center text-7xl font-black md:text-9xl">
+          Terms of Service
+        </h1>
+        <p className="text-center text-neutral-200">
+          Last updated: March 9, 2024
+        </p>
+      </div>
+      <p>
+        Welcome to Pollist! By accessing or using our website, mobile
+        application, or services (collectively, the &quot;Service&quot;), you
+        signify that you have read, understood, and agree to be bound by these
+        Terms and Conditions (&quot;Terms&quot;), which include our Privacy
+        Policy. If you do not agree with any of these terms, you are prohibited
+        from using or accessing this site.
+      </p>
+      <h2>Use of Service</h2>
+      <p>
+        The Service is intended for your personal, non-commercial use. Pollist
+        grants you a limited, non-exclusive, non-transferable, and revocable
+        license to access and use the Service for its intended purposes, subject
+        to your compliance with these Terms.
+      </p>
+      <h2>User-Generated Content</h2>
+      <p>
+        Users may post, upload, share, or store content, including messages,
+        text, photos, videos, and other materials (collectively, &quot;User
+        Content&quot;). You are solely responsible for your use of such features
+        and for the User Content that you post. You represent and warrant that
+        you own or have the necessary rights to all of your User Content and
+        that use of your User Content does not infringe, misappropriate, or
+        violate the rights of any third party.
+      </p>
+      <p>
+        While we strive to maintain high standards for quality and content, we
+        do not monitor or control the User Content posted and can make no
+        guarantees regarding its integrity, accuracy, or quality. We assume no
+        responsibility or liability for any User Content, including any loss or
+        damage resulting from it.
+      </p>
+      <h2>Acceptance of Terms</h2>
+      <p>
+        By accessing or using the Service, you agree to these Terms and consent
+        to our collection, use, and sharing of your information as outlined in
+        our Privacy Policy. If you do not agree to these Terms, you must not
+        access or use the Service.
+      </p>
+      <h2>Limitation of Liability</h2>
+      <p>
+        Pollist, its affiliates, and its service providers will not be liable
+        for any indirect, incidental, special, consequential, or punitive
+        damages, including loss of profits, data, use, goodwill, or other
+        intangible losses, resulting from (a) your access to or use of or
+        inability to access or use the Service; (b) any conduct or content of
+        any third party on the Service; (c) any content obtained from the
+        Service; and (d) unauthorized access, use, or alteration of your
+        transmissions or content, whether based on warranty, contract, tort
+        (including negligence), or any other legal theory, whether or not we
+        have been informed of the possibility of such damage.
+      </p>
+      <h2>Changes to Terms</h2>
+      <p>
+        We reserve the right, at our sole discretion, to modify or replace these
+        Terms at any time. If a revision is material, we will provide at least
+        30 days&apos; notice prior to any new terms taking effect. What
+        constitutes a material change will be determined at our sole discretion.
+      </p>
+      {/* <h2>Contact Us</h2>
+      <p>
+        If you have any questions about these Terms, please contact us at:
+        [Insert Contact Information].
+      </p> */}
+    </main>
+  );
+}
