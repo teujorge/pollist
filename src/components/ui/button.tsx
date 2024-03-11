1f737ae5e0e395d20d89b2f07e6baa375bebab24
Matheus Jorge <mrljorge@outlook.com>
2024-03-11 17:34:51 -0300

    style : use hovact instead of hover tailwind css in shadcn components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,15 +10,15 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow hovact:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground shadow-sm hovact:bg-destructive/80",
         outline:
-          "border border-accent bg-background shadow-sm hover:bg-accent/35 hover:text-accent-foreground",
+          "border border-accent bg-background shadow-sm hovact:bg-accent/35 hovact:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-secondary text-secondary-foreground shadow-sm hovact:bg-secondary/80",
+        ghost: "hovact:bg-accent hovact:text-accent-foreground",
+        link: "text-primary underline-offset-4 hovact:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
