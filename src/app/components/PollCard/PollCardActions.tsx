513e96f0546932951419d2bf49cac1257d7798ed
Matheus Jorge <mrljorge@outlook.com>
2024-03-12 23:58:42 -0300

    style : remove black border from vote option

--- src/app/components/PollCard/PollCardActions.tsx
+++ src/app/components/PollCard/PollCardActions.tsx
@@ -354,7 +354,7 @@ export function PollCardActions(props: PollCardActionsProps) {
                 "relative flex w-full cursor-pointer flex-row items-center gap-2 rounded-xl border px-4 py-2 transition-colors hovact:bg-neutral-900",
                 option.id === optionIdToHighlight
                   ? "border-primary"
-                  : "border-black",
+                  : "border-transparent",
               )}
             >
               <div
