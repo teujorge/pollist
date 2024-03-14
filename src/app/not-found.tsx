07d7a4e94dcdb6306543a64dd98c0d35aef5c3f1
Matheus Jorge <mrljorge@outlook.com>
2024-03-14 08:47:34 -0300

    add : home button in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
 export default function NotFoundPage() {
   return (
-    <main className="flex h-[calc(100dvh-64px)] w-full items-center justify-center">
-      <div className="flex w-fit flex-row items-center justify-center gap-2">
+    <main className="flex h-[calc(100dvh-64px)] w-full flex-col items-center justify-center gap-4">
+      <div className="flex w-fit flex-row items-center justify-center gap-4">
         <h1 className="text-3xl font-bold">404</h1>
         <div className="h-5 w-0.5 rounded-xl bg-neutral-400" />
         <p className="text-lg">Page not found</p>
       </div>
+      <Link
+        href="/"
+        className={buttonVariants({ variant: "outline", size: "lg" })}
+      >
+        Home
+      </Link>
     </main>
   );
 }
