57c5be24e060fd6ec18618acfc348131ccc3513f
Matheus Jorge <mrljorge@outlook.com>
2024-03-15 20:51:19 -0300

    fix : users layout

--- src/app/(with-auth)/users/layout.tsx
+++ src/app/(with-auth)/users/layout.tsx
@@ -2,17 +2,14 @@ import { UserPageProvider } from "./context";
 
 export default function UserPageLayout({
   children,
-  follows,
 }: {
   children: React.ReactNode;
-  follows: React.ReactNode;
 }) {
   return (
     <>
       <main className="flex w-full flex-col gap-2 overflow-hidden md:gap-4 ">
         <UserPageProvider>{children}</UserPageProvider>
       </main>
-      {follows}
     </>
   );
 }
