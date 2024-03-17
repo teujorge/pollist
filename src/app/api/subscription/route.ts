92c76ac24fedb9d213df420a93b0cb35d232be69
Matheus Jorge <mrljorge@outlook.com>
2024-03-17 20:40:13 -0300

    refactor : subscription webhook handler checks  current customer subscriptions, instead of assuming current event is the earliest... (with 50% chance of throwing for testing)

--- src/app/api/subscription/route.ts
+++ src/app/api/subscription/route.ts
@@ -15,6 +15,7 @@ export async function POST(req: NextRequest) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
   let event: Stripe.Event;
+
   try {
     const payload = await req.text();
     const signature = req.headers.get("stripe-signature");
@@ -46,7 +47,6 @@ export async function POST(req: NextRequest) {
     // Checkout completed -> i.e. user has paid
     case "checkout.session.completed": {
       const checkoutSessionCompleted = event.data.object;
-
       const userId = checkoutSessionCompleted.client_reference_id;
       const customerId = checkoutSessionCompleted.customer?.toString();
 
@@ -59,26 +59,24 @@ export async function POST(req: NextRequest) {
         });
       }
 
+      if (!customerId) {
+        const logMessage = `No customer ID found in checkout session ${checkoutSessionCompleted.id}`;
+        console.error(logMessage);
+        return NextResponse.json(null, {
+          status: 400,
+          statusText: logMessage,
+        });
+      }
+
       try {
-        // get tier from sub id
-        const subscription = await stripe.subscriptions.retrieve(
-          checkoutSessionCompleted.subscription as string,
-        );
-
-        const productId = subscription.items.data[0]?.price.id;
-
-        if (!productId) {
-          const logMessage = `No product ID found in subscription ${subscription.id}`;
-          console.error(logMessage);
-          return NextResponse.json(null, {
-            status: 400,
-            statusText: logMessage,
-          });
-        }
+        const productPriceId = await getProductPriceIdFromUser({
+          customerId,
+          stripe,
+        });
 
         await db.user.update({
           where: { id: userId },
-          data: { tier: getTier(productId), clerkId: customerId },
+          data: { tier: getTier(productPriceId), clerkId: customerId },
         });
       } catch (e) {
         let logMessage = `⚠️  Failed to update user ${userId}`;
@@ -98,21 +96,19 @@ export async function POST(req: NextRequest) {
       const subscriptionUpdated = event.data.object;
       const customerId = subscriptionUpdated.customer as string;
 
-      const productId = subscriptionUpdated.items.data[0]?.price.id;
-
-      if (!productId) {
-        const logMessage = `No product ID found in subscription ${subscriptionUpdated.id}`;
-        console.error(logMessage);
-        return NextResponse.json(null, {
-          status: 400,
-          statusText: logMessage,
-        });
-      }
-
       try {
+        const productPriceId = await getProductPriceIdFromUser({
+          customerId,
+          stripe,
+        });
+
+        // for testing
+        if (Math.random() < 0.5) throw new Error("Test error 😉");
+        // for testing
+
         await db.user.update({
           where: { clerkId: customerId },
-          data: { tier: getTier(productId) },
+          data: { tier: getTier(productPriceId) },
         });
       } catch (e) {
         let logMessage = `⚠️  Failed to update customer ${customerId}`;
@@ -142,9 +138,14 @@ export async function POST(req: NextRequest) {
       }
 
       try {
+        const productPriceId = await getProductPriceIdFromUser({
+          customerId,
+          stripe,
+        });
+
         await db.user.update({
           where: { clerkId: customerId },
-          data: { tier: "FREE" },
+          data: { tier: getTier(productPriceId) },
         });
       } catch (e) {
         let logMessage = `⚠️  Failed to update customer ${customerId} subscription in deleted event`;
@@ -167,17 +168,38 @@ export async function POST(req: NextRequest) {
   return NextResponse.json(null, { status: 200 });
 }
 
-function getTier(productId: string): SubTier {
-  const FREE_PRICE = process.env.STRIPE_SUB_FREE_PRICE;
-  const PRO_PRICE = process.env.STRIPE_SUB_PRO_PRICE;
+// Get the product ID from the user's subscription
+// Function assumes that the user has only one subscription
+async function getProductPriceIdFromUser({
+  customerId,
+  stripe,
+}: {
+  customerId: string;
+  stripe: Stripe;
+}) {
+  const subscriptions = await stripe.subscriptions.list({
+    customer: customerId,
+  });
 
-  if (productId === FREE_PRICE) {
-    return "FREE";
+  if (subscriptions.data.length === 0) {
+    throw Error(`No subscriptions found for customer ${customerId}`);
   }
 
-  if (productId === PRO_PRICE) {
-    return "PRO";
+  if (subscriptions.data.length > 1) {
+    throw Error(`Multiple subscriptions found for customer ${customerId}`);
   }
 
-  throw new Error(`Unknown product ID ${productId}`);
+  const productId = subscriptions.data[0]?.items.data[0]?.price.id;
+
+  return productId;
+}
+
+function getTier(productPriceId?: string): SubTier {
+  if (!productPriceId) return "FREE";
+
+  if (productPriceId === process.env.STRIPE_SUB_FREE_PRICE) return "FREE";
+
+  if (productPriceId === process.env.STRIPE_SUB_PRO_PRICE) return "PRO";
+
+  throw new Error(`Unknown product ID ${productPriceId}`);
 }
