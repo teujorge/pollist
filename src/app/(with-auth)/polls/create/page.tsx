da1c70c156308eb98caf175185647550f4369c6a
Matheus Jorge <mrljorge@outlook.com>
2024-03-18 20:49:19 -0300

    fix : create poll route (unintercepted) width overflow

--- src/app/(with-auth)/polls/create/page.tsx
+++ src/app/(with-auth)/polls/create/page.tsx
@@ -2,7 +2,7 @@ import { CreatePollForm } from "@/app/components/CrudPoll/CreatePollForm";
 
 export default function CreatePollPage() {
   return (
-    <main className="flex justify-center">
+    <main className="flex max-w-full justify-center">
       <CreatePollForm />
     </main>
   );
