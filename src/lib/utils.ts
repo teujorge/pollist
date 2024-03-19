ece75dbd043b994455dc819b48e03f35f3f66fed
Matheus Jorge <mrljorge@outlook.com>
2024-03-19 11:21:59 -0300

    fix : format numbers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatNumber(num: number): string {
+  const numberFormatter = new Intl.NumberFormat(undefined, {
+    style: "decimal",
+    notation: "compact",
+    maximumSignificantDigits: 3,
+  });
+
+  return numberFormatter.format(num);
 }
