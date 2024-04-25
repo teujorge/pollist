3878eda68fd072af25346a2a64fba51d04116e7e
Matheus Jorge <mrljorge@outlook.com>
2024-04-25 14:13:12 -0300

    fix : users page loading user image shimmer size

--- src/app/(with-auth)/users/[username]/loading.tsx
+++ src/app/(with-auth)/users/[username]/loading.tsx
@@ -5,7 +5,7 @@ export default function UserPageLoading() {
   return (
     <>
       <div className="flex w-full flex-row gap-8 px-3 py-3">
-        <div className="shimmer min-h-[100px] min-w-[100px] !rounded-full" />
+        <div className="shimmer max-h-[100px] min-h-[100px] min-w-[100px] max-w-[100px] !rounded-full" />
 
         <div className="flex flex-col justify-around gap-2">
           <div className="flex items-center gap-2">
