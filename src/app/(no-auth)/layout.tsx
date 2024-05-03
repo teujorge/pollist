2194d40b3430012968f0a1c50827f9944ecc283f
Matheus Jorge <mrljorge@outlook.com>
2024-05-03 18:27:10 -0300

    fix : hide whole static header in webview

--- src/app/(no-auth)/layout.tsx
+++ src/app/(no-auth)/layout.tsx
@@ -5,27 +5,25 @@ import { HideInWebView } from "../components/HideInWebView";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className="sticky left-0 right-0 top-0 z-40 flex w-full justify-between bg-gradient-to-b from-background from-60% px-5 py-4">
-        <div className="flex flex-row items-center gap-4">
-          <HideInWebView>
+      <HideInWebView>
+        <header className="sticky left-0 right-0 top-0 z-40 flex w-full justify-between bg-gradient-to-b from-background from-60% px-5 py-4">
+          <div className="flex flex-row items-center gap-4">
             <Link
               href="/"
               className="h-8 w-8 [&>svg>path]:transition-all [&>svg>path]:hovact:fill-primary [&>svg>path]:hovact:stroke-primary"
             >
               <IconSvg className="h-full w-full" />
             </Link>
-          </HideInWebView>
-        </div>
+          </div>
 
-        <div className="relative flex flex-row items-center gap-4 [&>a]:font-semibold">
-          <Link href="/about">About</Link>
-          <Link href="privacy">Privacy</Link>
-          <Link href="/tos">Terms</Link>
-          <HideInWebView>
+          <div className="relative flex flex-row items-center gap-4 [&>a]:font-semibold">
+            <Link href="/about">About</Link>
+            <Link href="privacy">Privacy</Link>
+            <Link href="/tos">Terms</Link>
             <Link href="/">Home</Link>
-          </HideInWebView>
-        </div>
-      </header>
+          </div>
+        </header>
+      </HideInWebView>
       {children}
     </>
   );
