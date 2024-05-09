35e383039adf763405b9be765931d5107681abf2
Matheus Jorge <mrljorge@outlook.com>
2024-05-09 13:39:36 -0300

    apple wins.......... why don't they allow us to send reference data to link notification to a user!

--- src/app/api/subscription/ios/route.ts
+++ src/app/api/subscription/ios/route.ts
@@ -1,11 +1,7 @@
-// import { db } from "@/server/prisma";
-import jwt from "jsonwebtoken";
+import { db } from "@/server/prisma";
 import { NextResponse } from "next/server";
 import {
-  Environment,
   NotificationType,
-  AppStoreServerAPI,
-  decodeRenewalInfo,
   decodeTransaction,
   decodeNotificationPayload,
   isDecodedNotificationDataPayload,
@@ -16,53 +12,17 @@ import type { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const KEY = Buffer.from(
-      process.env.IAP_SUBSCRIPTION_KEY!,
-      "base64",
-    ).toString("ascii");
-    const KEY_ID = process.env.IAP_KEY_ID!;
-    const ISSUER_ID = process.env.IAP_ISSUER_ID!;
     const APP_BUNDLE_ID = process.env.APNS_BUNDLE_ID!;
 
-    const api = new AppStoreServerAPI(
-      KEY,
-      KEY_ID,
-      ISSUER_ID,
-      APP_BUNDLE_ID,
-      Environment.Production,
-    );
+    const body = (await req.json()) as { signedPayload: string };
+    const signedPayload = body.signedPayload;
 
-    // Get the body of the request, which should contain the signedPayload from Apple
-    const _body = (await req.json()) as Record<string, string>;
-    console.log(Object.keys(_body));
-    for (const value of Object.values(_body)) {
-      console.log(`${value.slice(0, 50)}...`);
-    }
-
-    const body = _body as { signedPayload: string };
-    const { signedPayload } = body;
-
-    console.log("_body", _body);
-    console.log("body", body);
-    console.log("signedPayload", signedPayload);
-
-    // Decode the signed payload from Apple
     const payload = await decodeNotificationPayload(signedPayload);
-    const _payload = payload.data
-      ? {
-          ...payload,
-          data: {
-            ...payload.data,
-            signedRenewalInfo: "signedRenewalInfo",
-            signedTransactionInfo: "signedRenewalInfo",
-          },
-        }
-      : undefined;
-    console.log("Decoded Notification Payload:", _payload);
 
-    if (payload.data && payload.data.bundleId !== APP_BUNDLE_ID) {
+    if (!payload.data?.bundleId || payload.data?.bundleId !== APP_BUNDLE_ID) {
       console.error(
-        `Received notification for incorrect bundle ID: ${payload.data.bundleId}`,
+        "Received notification for incorrect bundle ID:",
+        payload.data?.bundleId,
       );
       return NextResponse.json(
         { error: "Incorrect bundle ID" },
@@ -70,21 +30,8 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    const data = {
-      ...payload.data,
-      signedRenewalInfo: undefined,
-      signedTransactionInfo: undefined,
-    };
-    console.log("Received Notification Payload Data:", data);
-    console.log("Received Notification Payload Summary:", payload.summary);
+    console.log("Decoded Notification Payload:", payload);
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-    const userId = (payload.data as any)?.applicationUsername as
-      | string
-      | undefined;
-    console.log("try userId =>", userId);
-
-    // Handle the notification based on its type
     if (isDecodedNotificationDataPayload(payload)) {
       console.log("Handling notification as DecodedNotificationDataPayload");
 
@@ -93,45 +40,30 @@ export async function POST(req: NextRequest) {
       );
       console.log("Transaction:", transaction);
 
-      const _transaction = _decodeTransaction(
-        payload.data.signedTransactionInfo,
-      );
-      console.log("_Transaction:", _transaction);
+      const dbTransaction = await db.appleTransaction.findUnique({
+        where: { originalTransactionId: transaction.originalTransactionId },
+      });
 
-      // Example for handling subscription statuses
-      const originalTransactionId = transaction.originalTransactionId;
-      const subscriptionResponse = await api.getSubscriptionStatuses(
-        originalTransactionId,
-      );
-
-      if (subscriptionResponse.data && subscriptionResponse.data.length > 0) {
-        const item = subscriptionResponse?.data[0]?.lastTransactions.find(
-          (item) => item.originalTransactionId === originalTransactionId,
+      if (!dbTransaction) {
+        console.error("Transaction not found in database");
+        return NextResponse.json(
+          { error: "Transaction not found" },
+          { status: 404 },
         );
-
-        if (item) {
-          const transactionInfo = await decodeTransaction(
-            item.signedTransactionInfo,
-          );
-          const renewalInfo = await decodeRenewalInfo(item.signedRenewalInfo);
-
-          console.log("--Transaction Info:", transactionInfo);
-          console.log("--Renewal Info:", renewalInfo);
-        }
       }
 
       switch (payload.notificationType) {
         case NotificationType.DidRenew: // Handle a successful renewal
         case NotificationType.Subscribed: // Handle a new subscription
         case NotificationType.OfferRedeemed: // Handle the redemption of a special offer
-          await activateSubscription("userId", "PRO");
+          await updateActiveSubscription(dbTransaction.userId, "PRO");
           break;
 
         case NotificationType.DidFailToRenew: // Handle a failed renewal attempt
         case NotificationType.Expired: // Handle subscription expiration
         case NotificationType.Revoke: // Handle a revocation of the subscription
         case NotificationType.Refund: // Handle a user getting a refund
-          await deactivateSubscription("userId");
+          await updateInactiveSubscription(dbTransaction.userId);
           break;
 
         case NotificationType.ConsumptionRequest: // Handle consumption of a consumable purchase
@@ -183,42 +115,25 @@ export async function POST(req: NextRequest) {
 }
 
 // Function to activate a subscription
-async function activateSubscription(userId: string, tier: SubTier) {
+async function updateActiveSubscription(userId: string, tier: SubTier) {
   console.log("Activating Subscription:", userId, tier);
-  // const user = await db.user.update({
-  //   where: { id: userId },
-  //   data: { tier: tier, ads: false },
-  // });
-  // console.log("Subscription Activated:", user.username, user.tier);
-  // return user;
+  const user = await db.user.update({
+    where: { id: userId },
+    data: { tier: tier, ads: false },
+  });
+
+  console.log("Subscription Activated:", user.username, user.tier);
+  return user;
 }
 
 // Function to deactivate a subscription
-async function deactivateSubscription(userId: string) {
+async function updateInactiveSubscription(userId: string) {
   console.log("Deactivating Subscription:", userId);
-  // const user = await db.user.update({
-  //   where: { id: userId },
-  //   data: { tier: "FREE", ads: true, private: false },
-  // });
-  // console.log("Subscription Deactivated:", user.username, user.tier);
-  // return user;
-}
+  const user = await db.user.update({
+    where: { id: userId },
+    data: { tier: "FREE", ads: true, private: false },
+  });
 
-function _decodeTransaction(signedTransactionInfo: string) {
-  const KEY = Buffer.from(process.env.IAP_SUBSCRIPTION_KEY!, "base64").toString(
-    "ascii",
-  );
-  const ISSUER_ID = process.env.IAP_ISSUER_ID!;
-
-  try {
-    const decoded = jwt.verify(signedTransactionInfo, KEY, {
-      algorithms: ["ES256"],
-      audience: "appstoreconnect-v1",
-      issuer: ISSUER_ID,
-    });
-    console.log("Decoded Transaction Info:", decoded);
-    return decoded;
-  } catch (error) {
-    console.error("Failed to decode transaction info:", error);
-  }
+  console.log("Subscription Deactivated:", user.username, user.tier);
+  return user;
 }
