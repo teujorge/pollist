cdbd61fb81d8a85943286ddebb76336e07a619ff
Matheus Jorge <mrljorge@outlook.com>
2024-05-09 14:32:28 -0300

    device endpoint, after purchase assume purchase... so activate PRO tier

--- src/app/api/subscription/device/route.ts
+++ src/app/api/subscription/device/route.ts
@@ -1,31 +1,31 @@
 import { db } from "@/server/prisma";
 import { NextResponse } from "next/server";
-import {
-  Environment,
-  AppStoreServerAPI,
-  decodeRenewalInfo,
-  decodeTransaction,
-} from "app-store-server-api";
+// import {
+// Environment,
+// AppStoreServerAPI,
+// decodeRenewalInfo,
+// decodeTransaction,
+// } from "app-store-server-api";
 import type { SubTier } from "@prisma/client";
 import type { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const KEY = Buffer.from(
-      process.env.IAP_SUBSCRIPTION_KEY!,
-      "base64",
-    ).toString("ascii");
-    const KEY_ID = process.env.IAP_KEY_ID!;
-    const ISSUER_ID = process.env.IAP_ISSUER_ID!;
-    const APP_BUNDLE_ID = process.env.APNS_BUNDLE_ID!;
+    // const KEY = Buffer.from(
+    //   process.env.IAP_SUBSCRIPTION_KEY!,
+    //   "base64",
+    // ).toString("ascii");
+    // const KEY_ID = process.env.IAP_KEY_ID!;
+    // const ISSUER_ID = process.env.IAP_ISSUER_ID!;
+    // const APP_BUNDLE_ID = process.env.APNS_BUNDLE_ID!;
 
-    const api = new AppStoreServerAPI(
-      KEY,
-      KEY_ID,
-      ISSUER_ID,
-      APP_BUNDLE_ID,
-      Environment.Production,
-    );
+    // const api = new AppStoreServerAPI(
+    //   KEY,
+    //   KEY_ID,
+    //   ISSUER_ID,
+    //   APP_BUNDLE_ID,
+    //   Environment.Production,
+    // );
 
     const { key, userId, originalTransactionId } = (await req.json()) as {
       key?: string;
@@ -75,43 +75,13 @@ export async function POST(req: NextRequest) {
       console.log("AppleTransaction Created:", dbTransaction);
     }
 
-    const subscriptionResponse = await api.getSubscriptionStatuses(
-      originalTransactionId,
-    );
-    console.log("Subscription Response:", subscriptionResponse);
-
-    if (subscriptionResponse.data && subscriptionResponse.data.length > 0) {
-      const item = subscriptionResponse.data[0]?.lastTransactions.find(
-        (item) => item.originalTransactionId === originalTransactionId,
-      );
-
-      if (item) {
-        const transactionInfo = await decodeTransaction(
-          item.signedTransactionInfo,
-        );
-        console.log("--Transaction Info:", transactionInfo);
-
-        const renewalInfo = await decodeRenewalInfo(item.signedRenewalInfo);
-        console.log("--Renewal Info:", renewalInfo);
-
-        // Check if subscription is 'active' and update the database accordingly
-        if (renewalInfo.autoRenewStatus) {
-          await activateSubscription(userId, "PRO");
-        } else {
-          await deactivateSubscription(userId);
-        }
-
-        return NextResponse.json(
-          { message: "Subscription status processed successfully" },
-          { status: 200 },
-        );
-      }
-    }
-
-    return NextResponse.json(
-      { error: "No subscription data found" },
-      { status: 404 },
-    );
+    await activateSubscription(userId, "PRO");
+    // const { signedTransactionInfo } = await api.getTransactionInfo(
+    //   originalTransactionId,
+    // );
+    // console.log("Signed Transaction Info:", signedTransactionInfo);
+    // const transactionInfo = await decodeTransaction(signedTransactionInfo);
+    // console.log("Decoded Transaction Info:", transactionInfo);
   } catch (e) {
     console.error("Error processing notification:", e);
     const errorMessage = e instanceof Error ? e.message : String(e);
@@ -133,13 +103,13 @@ async function activateSubscription(userId: string, tier: SubTier) {
   return user;
 }
 
-async function deactivateSubscription(userId: string) {
-  console.log("Deactivating Subscription:", userId);
-  const user = await db.user.update({
-    where: { id: userId },
-    data: { tier: "FREE", ads: true, private: false },
-  });
+// async function deactivateSubscription(userId: string) {
+//   console.log("Deactivating Subscription:", userId);
+//   const user = await db.user.update({
+//     where: { id: userId },
+//     data: { tier: "FREE", ads: true, private: false },
+//   });
 
-  console.log("Subscription Deactivated:", user.username, user.tier);
-  return user;
-}
+//   console.log("Subscription Deactivated:", user.username, user.tier);
+//   return user;
+// }
