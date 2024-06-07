a7245d9c6798ed2b3f51e6836423f0dbb510e248
Matheus Jorge <mrljorge@outlook.com>
2024-06-07 00:41:37 -0300

    try : force-static in notifications and create poll pages

--- src/app/(with-auth)/notifications/page.tsx
+++ src/app/(with-auth)/notifications/page.tsx
@@ -3,6 +3,8 @@
 import { PopoverProvider } from "@/app/hooks/usePopover";
 import { NotificationList } from "@/app/components/Header/NotificationsList";
 
+export const dynamic = "force-static";
+
 export default function NotificationsPage() {
   return (
     <main className="w-full">
--- src/app/(with-auth)/polls/create/page.tsx
+++ src/app/(with-auth)/polls/create/page.tsx
@@ -1,5 +1,7 @@
 import { CreatePollForm } from "@/app/components/CrudPoll/CreatePollForm";
 
+export const dynamic = "force-static";
+
 export default function CreatePollPage() {
   return (
     <main className="flex max-w-full justify-center">
