0172d7afcf2c088d8ad5ef47e2df33700ac28793
Matheus Jorge <mrljorge@outlook.com>
2024-06-07 00:45:47 -0300

    fix : remove force static

--- src/app/(with-auth)/polls/create/page.tsx
+++ src/app/(with-auth)/polls/create/page.tsx
@@ -1,7 +1,5 @@
 import { CreatePollForm } from "@/app/components/CrudPoll/CreatePollForm";
 
-export const dynamic = "force-static";
-
 export default function CreatePollPage() {
   return (
     <main className="flex max-w-full justify-center">
