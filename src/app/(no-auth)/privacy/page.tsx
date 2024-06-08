f69721de062a457953a427441d067a5352b3a1d1
Matheus Jorge <mrljorge@outlook.com>
2024-06-08 12:09:48 -0300

    update : privacy policy and tos

--- src/app/(no-auth)/privacy/page.tsx
+++ src/app/(no-auth)/privacy/page.tsx
@@ -8,9 +8,10 @@ export default function PrivacyPolicyPage() {
           Privacy Policy
         </h1>
         <p className="text-center text-accent-foreground">
-          Last updated: March 9, 2024
+          Last updated: June 8, 2024
         </p>
       </div>
+
       <p>
         This Privacy Policy describes how Pollist (&quot;we,&quot;
         &quot;us,&quot; or &quot;our&quot;) collects, uses, and shares
@@ -18,18 +19,24 @@ export default function PrivacyPolicyPage() {
         &quot;App&quot;). By using the App, you agree to the collection, use,
         and sharing of your information as described in this Privacy Policy.
       </p>
+
       <h2>Information We Collect</h2>
-      <p>
-        User Provided Information: We collect information you provide directly
-        to us when you create an account, such as your username, email address,
-        and other contact or profile information you choose to provide.
-      </p>
-      <p>
-        Automatically Collected Information: When you use our App, we
-        automatically collect certain information, including your IP address,
-        device type, operating system, and usage details (such as the time and
-        frequency of your use of the App).
-      </p>
+      <p>We collect the following types of information:</p>
+      <ul>
+        <li>
+          <strong>User Provided Information:</strong> We collect information you
+          provide directly to us when you create an account, such as your
+          username, email address, and other contact or profile information you
+          choose to provide.
+        </li>
+        <li>
+          <strong>Automatically Collected Information:</strong> When you use our
+          App, we automatically collect certain information, including your IP
+          address, device type, operating system, and usage details (such as the
+          time and frequency of your use of the App).
+        </li>
+      </ul>
+
       <h2>How We Use Your Information</h2>
       <p>We use the information we collect to:</p>
       <ul>
@@ -43,10 +50,11 @@ export default function PrivacyPolicyPage() {
         </li>
         <li>
           Detect, investigate, and prevent fraudulent transactions and other
-          illegal activities and protect the rights and property of Pollist and
+          illegal activities, and protect the rights and property of Pollist and
           others.
         </li>
       </ul>
+
       <h2>Sharing of Information</h2>
       <p>We may share your information as follows:</p>
       <ul>
@@ -70,52 +78,45 @@ export default function PrivacyPolicyPage() {
         Additionally, we may share aggregated or de-identified information,
         which cannot reasonably be used to identify you.
       </p>
-      <h2>Advertising and Analytics Services</h2>
-      <p>
-        We may allow others to provide analytics services and serve
-        advertisements on our behalf across the web and in mobile applications.
-        These entities may use cookies, web beacons, device identifiers, and
-        other technologies to collect information about your use of the App and
-        other websites and applications, including your IP address, web browser,
-        mobile network information, pages viewed, time spent on pages or in
-        apps, links clicked, and conversion information. This information may be
-        used by Pollist and others to, among other things, analyze and track
-        data, determine the popularity of certain content, deliver advertising
-        and content targeted to your interests on our services and other
-        websites, and better understand your online activity.
-      </p>
+
       <h2>Your Choices</h2>
-      <p>
-        Account Information: You may update, correct, or delete information
-        about you at any time by logging into your account and modifying your
-        information or by contacting us.
-      </p>
-      <p>
-        Cookies: Most web browsers are set to accept cookies by default. If you
-        prefer, you can usually choose to set your browser to remove or reject
-        browser cookies.
-      </p>
-      <p>
-        Promotional Communications: You can opt out of receiving promotional
-        emails from us by following the instructions in those emails. If you opt
-        out, we may still send you non-promotional emails, such as those about
-        your account or our ongoing business relations.
-      </p>
+      <ul>
+        <li>
+          <strong>Account Information:</strong> You may update, correct, or
+          delete information about you at any time by logging into your account
+          and modifying your information or by contacting us.
+        </li>
+        <li>
+          <strong>Cookies:</strong> Most web browsers are set to accept cookies
+          by default. If you prefer, you can usually choose to set your browser
+          to remove or reject browser cookies.
+        </li>
+        <li>
+          <strong>Promotional Communications:</strong> You can opt out of
+          receiving promotional emails from us by following the instructions in
+          those emails. If you opt out, we may still send you non-promotional
+          emails, such as those about your account or our ongoing business
+          relations.
+        </li>
+      </ul>
+
       <h2>Data Security</h2>
       <p>
         We take reasonable measures to help protect information about you from
         loss, theft, misuse, and unauthorized access, disclosure, alteration,
         and destruction.
       </p>
+
       <h2>Changes to the Privacy Policy</h2>
       <p>
-        We may update this Privacy Policy from time to time. If we make changes,
-        we will notify you by revising the date at the top of the policy and, in
-        some cases, we may provide you with additional notice (such as adding a
-        statement to our homepage or sending you a notification).
+        We reserve the right to modify or replace this Privacy Policy at any
+        time at our sole discretion. If we make changes, we will notify you by
+        revising the date at the top of the policy and, in some cases, we may
+        provide you with additional notice (such as adding a statement to our
+        homepage or sending you a notification). Your continued use of the App
+        following the posting of any changes to the Privacy Policy constitutes
+        acceptance of those changes.
       </p>
-      {/* <h2>8. Contact Us</h2>
-        <p>If you have any questions about this Privacy Policy, please contact us at: [Insert Contact Information].</p> */}
     </main>
   );
 }
--- src/app/(no-auth)/tos/page.tsx
+++ src/app/(no-auth)/tos/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export default function TermsOfServicePage() {
   return (
@@ -8,9 +9,10 @@ export default function TermsOfServicePage() {
           Terms of Service
         </h1>
         <p className="text-center text-accent-foreground">
-          Last updated: March 9, 2024
+          Last updated: June 8, 2024
         </p>
       </div>
+
       <p>
         Welcome to Pollist! By accessing or using our website, mobile
         application, or services (collectively, the &quot;Service&quot;), you
@@ -19,37 +21,62 @@ export default function TermsOfServicePage() {
         Policy. If you do not agree with any of these terms, you are prohibited
         from using or accessing this site.
       </p>
+
       <h2>Use of Service</h2>
       <p>
-        The Service is intended for your personal, non-commercial use. Pollist
-        grants you a limited, non-exclusive, non-transferable, and revocable
-        license to access and use the Service for its intended purposes, subject
-        to your compliance with these Terms.
+        Pollist grants you a limited, non-exclusive, non-transferable, and
+        revocable license to access and use the Service for personal,
+        non-commercial purposes. You agree to use the Service in compliance with
+        all applicable laws and regulations.
       </p>
+
       <h2>User-Generated Content</h2>
       <p>
         Users may post, upload, share, or store content, including messages,
         text, photos, videos, and other materials (collectively, &quot;User
         Content&quot;). You are solely responsible for your use of such features
-        and for the User Content that you post. You represent and warrant that
-        you own or have the necessary rights to all of your User Content and
-        that use of your User Content does not infringe, misappropriate, or
-        violate the rights of any third party.
-      </p>
-      <p>
+        and for the User Content that you post. By posting User Content, you
+        represent and warrant that you own or have the necessary rights to all
+        of your User Content and that use of your User Content does not
+        infringe, misappropriate, or violate the rights of any third party.
+        <br /> <br />
         While we strive to maintain high standards for quality and content, we
         do not monitor or control the User Content posted and can make no
-        guarantees regarding its integrity, accuracy, or quality. We assume no
+        guarantees regarding its integrity, accuracy, or quality. We reserve the
+        right to remove any User Content that we deem to be in violation of
+        these Terms or otherwise harmful to the Service, but we assume no
         responsibility or liability for any User Content, including any loss or
         damage resulting from it.
       </p>
+
+      <h2>Disclaimer of Warranties</h2>
+      <p>
+        The Service is provided &quot;as is&quot; and &quot;as available&quot;
+        with all faults and without warranty of any kind. Pollist hereby
+        disclaims all warranties and conditions with respect to the Service,
+        either express, implied, or statutory, including, but not limited to,
+        the implied warranties and/or conditions of merchantability, of
+        satisfactory quality, of fitness for a particular purpose, of accuracy,
+        of quiet enjoyment, and of non-infringement of third-party rights. No
+        oral or written information or advice given by Pollist or its authorized
+        representative shall create a warranty.
+      </p>
+
       <h2>Acceptance of Terms</h2>
       <p>
         By accessing or using the Service, you agree to these Terms and consent
         to our collection, use, and sharing of your information as outlined in
-        our Privacy Policy. If you do not agree to these Terms, you must not
-        access or use the Service.
+        our{" "}
+        <Link
+          href="/privacy-policy"
+          className="text-primary underline decoration-transparent transition-all hovact:decoration-primary"
+        >
+          Privacy Policy
+        </Link>
+        . If you do not agree to these Terms, you must not access or use the
+        Service.
       </p>
+
       <h2>Limitation of Liability</h2>
       <p>
         Pollist, its affiliates, and its service providers will not be liable
@@ -63,18 +90,44 @@ export default function TermsOfServicePage() {
         (including negligence), or any other legal theory, whether or not we
         have been informed of the possibility of such damage.
       </p>
+
       <h2>Changes to Terms</h2>
       <p>
-        We reserve the right, at our sole discretion, to modify or replace these
-        Terms at any time. If a revision is material, we will provide at least
-        30 days&apos; notice prior to any new terms taking effect. What
-        constitutes a material change will be determined at our sole discretion.
+        We reserve the right to modify or replace these Terms at any time at our
+        sole discretion. If we make changes, we will notify you by revising the
+        date at the top of the policy and, in some cases, we may provide you
+        with additional notice (such as adding a statement to our homepage or
+        sending you a notification). Your continued use of the Service following
+        the posting of any changes to the Terms constitutes acceptance of those
+        changes.
       </p>
-      {/* <h2>Contact Us</h2>
+      <h2>Termination</h2>
       <p>
-        If you have any questions about these Terms, please contact us at:
-        [Insert Contact Information].
-      </p> */}
+        These Terms are effective until terminated by you or Pollist. Your
+        rights under these Terms will terminate automatically if you fail to
+        comply with any of its terms.
+      </p>
+      <h2>Governing Law</h2>
+      <p>
+        These Terms and the relationship between you and Pollist shall be
+        governed by the laws of the State of California, excluding its conflicts
+        of law provisions. You and Pollist agree to submit to the personal and
+        exclusive jurisdiction of the courts located within the county of Santa
+        Clara, California, to resolve any dispute or claim arising from these
+        Terms.
+      </p>
+      <h2>Miscellaneous</h2>
+      <p>
+        If any provision of these Terms is held to be invalid or unenforceable,
+        such provision shall be struck and the remaining provisions shall be
+        enforced to the fullest extent under law. Pollist&apos;s failure to
+        enforce any right or provision in these Terms will not constitute a
+        waiver of such right or provision unless acknowledged and agreed to by
+        us in writing.
+        <br /> <br />
+        By using the Service, you agree to these Terms and acknowledge that you
+        have read and understood them.
+      </p>
     </main>
   );
 }
