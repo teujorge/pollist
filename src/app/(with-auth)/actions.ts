e0faa2cc78675b0b54a1faea6a82ea41ddd61e92
Matheus Jorge <mrljorge@outlook.com>
2024-06-10 15:27:05 -0300

    refactor : firebase env vars

--- src/app/(with-auth)/actions.ts
+++ src/app/(with-auth)/actions.ts
@@ -253,13 +253,13 @@ export async function silentlyUpdateAPN() {
 }
 
 import admin from "firebase-admin";
-import type { ServiceAccount } from "firebase-admin";
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY!,
-) as ServiceAccount;
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
 });
 
 async function sendFCM({
