08f2cdd362b90ea8a2c50557d4f62684da11c468
Matheus Jorge <mrljorge@outlook.com>
2024-06-10 15:51:48 -0300

    fix : firebase private key new line char

--- src/app/(with-auth)/actions.ts
+++ src/app/(with-auth)/actions.ts
@@ -254,13 +254,16 @@ export async function silentlyUpdateAPN() {
 
 import admin from "firebase-admin";
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-  }),
-});
+if (!admin.apps.length) {
+  // This check prevents initializing the app multiple times.
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // Ensure new lines are correctly interpreted
+    }),
+  });
+}
 
 async function sendFCM({
   title,
