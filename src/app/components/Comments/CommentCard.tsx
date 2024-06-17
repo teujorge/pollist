848ca8f85445dde7a5c43f5d25d53f0ce75cea36
Matheus Jorge <mrljorge@outlook.com>
2024-06-17 13:00:02 -0300

    fix : hide comment actions when comment is 'deleted'

--- src/app/components/Comments/CommentCard.tsx
+++ src/app/components/Comments/CommentCard.tsx
@@ -105,7 +105,7 @@ export function CommentCard({
             </span>
           </p>
 
-          <CommentCardActions />
+          {!comment.deleted && <CommentCardActions />}
 
           {showPurpleForUnreadComment && (
             <TriggerNotificationSeen
